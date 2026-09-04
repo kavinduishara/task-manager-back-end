9aa2e672f9ac94783b0b31b9b18e541cdc70975b
kavindu ishara <kavinduishara2923@gmail.com>
2026-09-04 09:30:53 +0530

    test with mock data

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,5 +1,10 @@
+import { seedData } from "../../seed.js";
+
 export const getAllTasks=async (req, res)=>{
-    res.json({ message: "Tasks retrieved successfully",});
+
+    res.json({ message: "All tasks fetched successfully",
+        data: seedData
+    });
 }
 
 export const addTasks=async (req, res)=>{
