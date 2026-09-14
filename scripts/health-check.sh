a507764bb8447f78c5c37b5397fd44c0f5606c6f
kavindu ishara <kavinduishara2923@gmail.com>
2026-09-14 21:00:56 +0530

    health check bash file update

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -2,6 +2,25 @@
 
 set -e
 
-df -h
+MEMORY=$(free |grep "Mem" | awk '{ print 100*$7/$2 }')
+DISK=$(df --total |grep "total"|awk '{print $5}')
+echo $DISK
+echo $MEMORY
+if systemctl is-active --quiet nginx ;then
+        echo "nginx running"
+else
+        echo "nginx not active"
+fi
 
-free -h
+if curl -fs http://localhost:3000/login > /dev/null; then
+        echo "Frontend: OK"
+else
+        echo "Frontend: FAILED"
+fi
+
+
+if curl -fs http://localhost:3001/api/health > /dev/null; then
+        echo "Backend: OK"
+else
+        echo "Backend: FAILED"
+fi
